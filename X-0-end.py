0b19ad46cf7669799d3da72cbc834b09f3a173ef
Dimvn1964 <98300519+Dimvn1964@users.noreply.github.com>
2022-01-24 19:37:38 +0300

    Add files via upload

--- X-0-end.py
+++ X-0-end.py
@@ -1,11 +1,14 @@
+def dia_coor():
+    print("Диапазон каждой координаты от 0 до 2.")
+    print('Окончание ввода - клавиша "Enter".')
+
 def welcom():
     print("---------------")
     print('Рады приветствовать Вас в игре')
     print('       "Крестики-нолики".')
     print("Формат ввода:")
     print("первая цифра - номер строки, вторая цифра - номер столбца.")
-    print("Диапазон каждой координаты от 0 до 2.")
-    print('Окончание ввода - клавиша "Enter".')
+    dia_coor()
     print("--------------")
 
 def quad():
@@ -23,24 +26,36 @@ def ques():
 
         if len(cords) != 2:
             print("Введите обе координаты!")
+            dia_coor()
             continue
 
         x, y = cords
 
         if not(x.isdigit()) or not(y.isdigit()):
             print("Необходимо ввести числа от 0 до 2!")
+            dia_coor()
             continue
 
         x, y = int(x), int(y)
 
-        if 0 > x or 0 > y or x > 2 or y > 2 :
+        if 0 > x or 0 > y or x > 2 or y > 2:
             print("Координаты вне диапазона! ")
+            dia_coor()
             continue
 
-        if field[x][y] != " "  :
+        if field[x][y] != " ":
             print(" Клетка занята! ")
             continue
+
         return x, y
+
+def win_X():
+       quad()
+       print("Выиграл X. Поздравляем !!!")
+
+def win_0():
+        quad()
+        print("Выиграл 0. Поздравляем !!!")
 
 def check():
     for i in range(3):
@@ -48,42 +63,41 @@ def check():
         for j in range(3):
             sym.append(field[i][j])
         if sym == ["X", "X", "X"]:
-            print("Выиграл X. Поздравляем !!!")
+            win_X()
             return True
         if sym == ["0", "0", "0"]:
-            print("Выиграл 0. Поздравляем !!!")
+            win_0()
             return True
-
 
     for i in range(3):
         sym = []
         for j in range(3):
             sym.append(field[j][i])
         if sym == ["X", "X", "X"]:
-            print("Выиграл X. Поздравляем !!!")
+            win_X()
             return True
         if sym == ["0", "0", "0"]:
-            print("Выиграл 0. Поздравляем !!!")
+            win_0()
             return True
 
     sym = []
     for i in range(3):
         sym.append(field[i][i])
     if sym == ["X", "X", "X"]:
-        print("Выиграл X. Поздравляем !!!")
+        win_X()
         return True
     if sym == ["0", "0", "0"]:
-        print("Выиграл 0. Поздравляем !!!")
+        win_0()
         return True
 
     sym = []
     for i in range(3):
         sym.append(field[i][2-i])
     if sym == ["X", "X", "X"]:
-        print("Выиграл X. Поздравляем !!!")
+        win_X()
         return True
     if sym == ["0", "0", "0"]:
-        print("Выиграл 0. Поздравляем !!!")
+        win_0()
         return True
     return False
 
@@ -97,10 +111,10 @@ while True:
 
     if count % 2 == 1:
         print("Ходит X. Введите через пробел номер строки и номер столбца.")
-        print('Диапазон каждой координаты от 0 до 2. Окончание ввода - клавиша "Enter".')
+        dia_coor()
     else:
         print("Ходит 0. Введите через пробел номер строки и номер столбца.")
-        print('Диапазон каждой координаты от 0 до 2. Окончание ввода - клавиша "Enter".')
+        dia_coor()
 
     x, y = ques()
 
